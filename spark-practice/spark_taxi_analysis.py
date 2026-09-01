26fb72d6927dcf0827abafc7dcc222b968bca007
Luffen <z.roma1337@gmail.com>
2026-09-02 03:17:35 +0500

    v1.2 Update spark file

--- spark-practice/spark_taxi_analysis.py
+++ spark-practice/spark_taxi_analysis.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, count, col
+from pyspark.sql.functions import avg, count, col, round as spark_round
 
 
 spark = (
@@ -11,6 +11,11 @@ spark = (
 
 
 df = spark.read.parquet("yellow_tripdata_2023-12.parquet")
+
+
+vendor_stats =(
+    df.groupBy()
+)
 
 
 print(f"Total rows: {df.count()}")
