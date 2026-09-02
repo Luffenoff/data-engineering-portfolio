b80e660b60e1c28fff2a85a3ea8d712168540f69
Luffen <z.roma1337@gmail.com>
2026-09-03 02:37:43 +0500

    v1.4.1: final test problem asyncio file

--- sync_problem.py
+++ sync_problem.py
@@ -2,4 +2,15 @@ import time
 
 
 def slow_task(name, delay):
-    print
+    print(f"[{name}] Начал работу")
+    time.sleep(delay) # симуляция ожидания ответа
+    print(f"[{name}] Закончил за {delay} сек")
+    
+start = time.time()
+
+slow_task("Задача 1", 2)
+slow_task("Задача 2", 2)
+slow_task("Задача 3", 2)
+
+
+print(f"Всего заняло {time.time() - start:.2f} сек")
