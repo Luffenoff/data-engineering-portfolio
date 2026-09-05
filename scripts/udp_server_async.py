c06a4654368aa00b02441baf253b8deaec5a67f2
Luffen <z.roma1337@gmail.com>
2026-09-05 23:53:19 +0500

    v1.4.5:final asyncio file udp-server

--- scripts/udp_server_async.py
+++ scripts/udp_server_async.py
@@ -10,3 +10,25 @@ class TelemetryProtocol(asyncio.DatagramProtocol):
     def datagram_received(self, data, addr):
         message = data.decode('utf-8')
         print(f"Получено от {addr}: {message}")
+        # Асинх
+        asyncio.create_task(self.process_message(message, addr))
+        
+        
+    async def process_message(self, message, addr):
+        await asyncio.sleep(0.1)
+        print(f" -> Обработано сообщение от {addr}")
+        
+    
+async def main():
+    loop = asyncio.get_event_loop()
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: TelemetryProtocol(),
+        local_addr=("127.0.0.1", 9999)
+    )
+    try:
+        await asyncio.sleep(3600)
+    finally:
+        transport.close()
+        
+asyncio.run(main())
+        
