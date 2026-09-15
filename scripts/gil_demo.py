d4946c86ead730b9244396d510a74ef0feb3faf1
Luffen <z.roma1337@gmail.com>
2026-09-15 21:07:21 +0500

    v1.5.4:add multiprocessing in gil_demo

--- scripts/gil_demo.py
+++ scripts/gil_demo.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import multiprocessing
 
 
 def cpu_bound_task(n):
@@ -13,17 +14,28 @@ def cpu_bound_task(n):
 N = 50_000_000
 
 
-start = time.time()
-cpu_bound_task(N)
-cpu_bound_task(N)
-print(f"Последовательно (2 задачи): {time.time() - start:.2f} сек")
+if __name__ == "__main__":
+    start = time.time()
+    cpu_bound_task(N)
+    cpu_bound_task(N)
+    print(f"Последовательно (2 задачи): {time.time() - start:.2f} сек")
 
 
-# start = time.time()
-# t1 = threading.Thread(target=cpu_bound_task, args=(N,))
-# t2 = threading.Thread(target=cpu_bound_task, args=(N,))
-# t1.start()
-# t2.start()
-# t1.join()
-# t2.join()
-# print(f"В двух потоках: {time.time() - start:.2f} сек")
+    start = time.time()
+    t1 = threading.Thread(target=cpu_bound_task, args=(N,))
+    t2 = threading.Thread(target=cpu_bound_task, args=(N,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    print(f"В двух потоках: {time.time() - start:.2f} сек")
+    
+    start = time.time()
+    p1 = multiprocessing.Process(target=cpu_bound_task, args=(N,))
+    p2 = multiprocessing.Process(target=cpu_bound_task, args=(N,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    print(f"В двух процессах: {time.time() - start:.2f} сек"
+          )
